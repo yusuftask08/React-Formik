2fdf72027948d58406ea68c22ff9d3d7cf6c560b
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-20 12:54:00 +0300

    Form Validasyonları - 2

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import { useFormik } from 'formik';
 import validationSchema from './valid';
 function Signup() {
-    const { handleChange, handleSubmit, values } = useFormik({
+    const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
         initialValues: {
             email: '',
             password: '',
@@ -18,15 +18,19 @@ function Signup() {
                 <h1>Sign Up</h1>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="email">Email</label>
-                    <input name="email" value={values.email} onChange={handleChange} />
+                    <input name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.email && touched.email && <div className='error'>{errors.email}</div>}
                     <br />
                     <br />
                     <label htmlFor="password">Password</label>
-                    <input name="password" value={values.password} onChange={handleChange} />
+                    <input name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.password && touched.password && <div className='error'>{errors.password}</div>}
                     <br />
                     <br />
                     <label htmlFor="passwordConfirm">Confirm Password </label>
-                    <input name="passwordConfirm" value={values.passwordConfirm} onChange={handleChange} />
+                    <input name="passwordConfirm" value={values.passwordConfirm} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.passwordConfirm && touched.passwordConfirm && <div className='error'>{errors.passwordConfirm}</div>}
+
                     <br />
                     <br />
                     <button type="submit">Submit</button>
--- src/components/valid.js
+++ src/components/valid.js
@@ -2,9 +2,9 @@ import { object, string } from 'yup';
 import * as yup from 'yup'
 const userSchema = object({
 
-    email: string().email().required(),
-    password: string().min(5).required(),
-    passwordConfirm: string().oneOf([yup.ref('password')]).required()
+    email: string().email('geçerli bir email giriniz.').required('bu alan zorunludur'),
+    password: string().min(5, 'parolanız en az 5 karakter olmalıdır.').required('bu alan zorunludur'),
+    passwordConfirm: string().oneOf([yup.ref('password')], 'parolanız eşleşmiyor').required('bu alan zorunludur'),
 });
 
 export default userSchema
