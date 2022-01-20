0ce0d4cca058991c13c8e26898912c53a11709e9
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-20 10:57:18 +0300

    handleSubmit & handleChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Formik, Field, Form } from 'formik';
+import { Formik } from 'formik';
 
 function App() {
   return (
@@ -17,25 +17,26 @@ function App() {
             console.log(JSON.stringify(values, null, 2));
           }}
         >
-          <Form>
-            <label htmlFor="firstName">First Name</label>
-            <Field id="firstName" name="firstName" placeholder="Jane" />
-            <br />
-            <br />
-            <label htmlFor="lastName">Last Name</label>
-            <Field id="lastName" name="lastName" placeholder="Doe" />
-            <br />
-            <br />
-            <label htmlFor="email">Email</label>
-            <Field
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            <br /><br />
-            <button type="submit">Submit</button>
-          </Form>
+          {
+            ({ handleSubmit, handleChange }) =>
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input name="firstName" onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="lastName">Last Name</label>
+                <input name="lastName" onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="email">Email</label>
+                <input
+                  name="email"
+                  type="email" onChange={handleChange}
+                />
+                <br /><br />
+                <button type="submit">Submit</button>
+              </form>
+          }
         </Formik>
       </header>
     </div>
